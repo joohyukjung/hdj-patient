9173198a70a56a3fb70ffa2b3911b35f2942cba7
joohyukjung <joohyukjung@gmail.com>
2024-03-04 17:12:32 +0900

    웹 프로젝트 구성

---
+++ src/main/java/com/example/hdjpatient/HdjPatientApplication.java
@@ -0,0 +1,13 @@
+package com.example.hdjpatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdjPatientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HdjPatientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hdjpatient/HdjPatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hdjpatient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdjPatientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
